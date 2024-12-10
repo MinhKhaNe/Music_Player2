48496a6da616cc8bd50455d4bb271495e8b9bfa4
MinhKhaNe <jacknguyen300804@gmail.com>
2024-12-10 11:23:03 +0700

    Image Action

--- src/App.java
+++ src/App.java
@@ -5,11 +5,11 @@ public class App {
         SwingUtilities.invokeLater(new Runnable(){
             @Override
             public void run(){
-                new MusicPlayerGUI().setVisible(true);
+                new StartScreen().setVisible(true);
 
-                Song song = new Song("src/assets/Wind Riders - Asher Fulero.mp3");
-                System.out.println(song.getSongTitle());
-                System.out.println(song.getSongArtist());
+                //Song song = new Song("src/assets/Wind Riders - Asher Fulero.mp3");
+                //System.out.println(song.getSongTitle());
+                //System.out.println(song.getSongArtist());
             }
         });
     }
---
+++ src/StartScreen.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class StartScreen extends JFrame {
+
+    public StartScreen() {
+        setTitle("");
+        setSize(400, 600);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        getContentPane().setBackground(Color.BLACK);
+
+        ImageIcon beginImage = new ImageIcon("src/image/avatar.png");
+        JLabel imageLabel = new JLabel(beginImage, JLabel.CENTER);
+        add(imageLabel, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 20));
+        buttonPanel.setBackground(Color.BLACK);
+        JButton startButton = new JButton("Click anywhere to start");
+        startButton.setFont(new Font("Dialog", Font.PLAIN, 24));
+        startButton.setBackground(Color.BLACK);
+        startButton.setForeground(Color.WHITE);
+        startButton.setBorder(BorderFactory.createEmptyBorder());
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Mở MusicPlayerGUI
+                new MusicPlayerGUI().setVisible(true);
+                dispose(); //
+            }
+        });
+
+        buttonPanel.add(startButton);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+}
+
+
